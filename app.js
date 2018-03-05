4a6280e38abed797d44a3c3d2453ee6e6d771a0d
prasanth.s <navoshanth@gmail.com>
2018-03-05 12:52:32 +0530

    Simple change

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.get('/', (req, res) => {
    // res.send('Hello REFE!')
+   res.status(200).json({
+       message: 'It works as expected'
+   })
 })
+app.post('/', (req, res) => {
+    // res.send('Hello REFE!')
+    res.status(200).json({
+        message: 'Post'
+    })
+ })
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
